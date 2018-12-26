a7cab6228abb358927699d28fdd865263b3c9c74
rylynn <575568920@qq.com>
2018-12-26 15:37:11 +0800

    visual tool

--- config.py
+++ config.py
@@ -9,4 +9,6 @@ class Config(object):
     detection_head_input_size = (7, 7)
     data_dir = r"F:\Code\Yolov1\dataset"
 
+    shape_bbox_colors = ((179, 238, 58),(205, 0, 0), (205, 105, 201))
+
 cfg = Config()
---
+++ util.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def plot_image(img):
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
+def plot_grid(imgs, n_row=3, n_col=3):
+    _, figs = plt.subplots(n_row, n_col)
+
+    for i in range(n_row):
+        for j in range(n_col):
+            figs[i][j].imshow(imgs[i*n_row + j])
+            figs[i][j].axis('off')
+    plt.show()
+
+def plot_bbox(img, bboxes, labels, label_to_color):
+    # Attention!!!!
+    img = img.copy()
+    for i in range(len(bboxes)):
+        cv2.rectangle(img, (bboxes[i, 0], bboxes[i, 1]), (bboxes[i, 2], bboxes[i, 3]), label_to_color[labels[i]], 1)
+    return img
+
